edbbb578491caff6d3b68db9e4c2925192dfaf69
Tandin Wangchen <taandinwangchen27@icloud.com>
2023-07-03 20:21:24 +0530

    chnages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Update this line
     port: 3000,
   },
-  // build: {
-  //   outDir: 'build',
-  // },
 });
