995f901362463c6bce7fc11bcb50bf62a12991d1
Tandin Wangchen <taandinwangchen27@icloud.com>
2023-07-03 18:32:28 +0530

    initialized the project

---
+++ src/assets/index.js
@@ -0,0 +1,89 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import website from "./website.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import mababy from "./mababy.png";
+import xtra from "./4xtra.png";
+import smartscience from "./smartscience2.png";
+import rotunda from "./rotunda.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import python from "./tech/python.png";
+import keycloak from "./tech/keycloak.png";
+import gcp from "./tech/gcp.png";
+import aws from "./tech/aws.png";
+import jenkins from "./tech/jenkins.png";
+import larvel from "./tech/larvel.png";
+import cplus from "./tech/cplus.png";
+import c from "./tech/c.png";
+import gitlab from "./tech/gitlab.png";
+import bitucket from "./tech/bitucket.png";
+
+
+import synapsys from "./company/synapsys.png";
+import trabeya from "./company/trabeya.png";
+import booleanlabs from "./company/booleanlabs.png";
+import axcito from "./company/axcito.png";
+import sliit from "./company/sliit.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  python,
+  keycloak,
+  gcp,
+  aws,
+  jenkins,
+  larvel,
+  cplus,
+  c,
+  gitlab,
+  bitucket,
+  synapsys,
+  trabeya,
+  booleanlabs,
+  axcito,
+  sliit,
+  website,
+  mababy,
+  xtra,
+  smartscience,
+  rotunda
+};
---
+++ src/constants/index.js
@@ -0,0 +1,349 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  reactjs,
+  nodejs,
+  mongodb,
+  git,
+  docker,
+  synapsys,
+  trabeya,
+  booleanlabs,
+  axcito,
+  sliit,
+  python,
+  gcp,
+  aws,
+  jenkins,
+  larvel,
+  c,
+  mababy,
+  xtra,
+  smartscience,
+  rotunda,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Software Engineer",
+    icon: web,
+  },
+  {
+    title: "R&D",
+    icon: backend,
+  },
+  {
+    title: "Full Stack",
+    icon: mobile,
+  },
+  {
+    title: "AI Researcher",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "C",
+    icon: c,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Larvel",
+    icon: larvel,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "Google Cloud Platform",
+    icon: gcp,
+  },
+  {
+    name: "AWS",
+    icon: aws,
+  },
+  
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name: "Jenkins",
+    icon: jenkins,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "Software Engineer",
+    company_name: "Synapsys ltd",
+    icon: synapsys,
+    iconBg: "#FFFFFF",
+    date: "Jul 2022 - Present",
+    points: [
+      "Develop and maintain web applications using React.js, ensuring high-quality code and optimal performance.",
+      "Implement graphs and data visualization techniques using React.js frameworks.",
+      "Handle and manipulate scientific data efficiently, ensuring accuracy and reliability.",
+      "Connect and integrate external APIs (JSON) to fetch and display data within React.js applications.",
+      "Follow Git structure and best practices for version control, branching, and collaboration with other developers.",
+      "Structure the React.js DOM (Document Object Model) efficiently, ensuring proper component composition and organization.",
+    ],
+  },
+  {
+    title: "Backend Engineer",
+    company_name: "Trabeya",
+    icon: trabeya,
+    iconBg: "#E6DEDD",
+    date: "Sep 2021 - Apr 2022",
+    points: [
+      "Develop and maintain Node.js applications, ensuring efficient and scalable server-side logic.",
+      "Integrate and interact with MongoDB databases for data storage and retrieval.",
+      "Implement user authentication and authorization using Keycloak, ensuring secure access control.",
+      "Containerize applications using Docker for easy deployment and scalability.",
+      "Set up and configure Jenkins for continuous integration and automated testing.",
+      "Utilize GitHub for version control, collaborating with other developers and managing code repositories.",
+      "Design and implement trilateration algorithms using RSSI measurements from Bluetooth beacons to calculate precise location information.",
+      "Work with IoT devices and technologies to gather data, perform analytics, and enable communication between devices and systems.",
+      "Create comprehensive API documentation, including endpoint specifications, request/response examples, and usage guidelines.",
+      "Collaborate with cross-functional teams to define API requirements and ensure accurate documentation.",
+      "Keep API documentation up-to-date with any changes or enhancements to the API.",
+      "Provide support and guidance to other developers on API implementation and usage.",
+    ],
+  },
+  
+  {
+    title: "Research Assistant",
+    company_name: "SLIIT",
+    icon: sliit,
+    iconBg: "#FFFFFF",
+    date: "Mar 2022 - Apr 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Identify and address accuracy loss in AI models by manipulating and optimizing nodes within the model architecture.",
+      "Analyze data and perform statistical analysis to gain insights and improve AI model performance.",
+      "Design and implement experiments to evaluate the effectiveness and efficiency of AI models.",
+      "Develop and deploy AI models using appropriate frameworks and libraries.",
+      "Optimize AI models for performance, scalability, and resource efficiency.",
+      "Document research findings, methodologies, and experiments for future reference and knowledge sharing.",
+      "Continuously monitor and evaluate the performance of AI models, making adjustments as necessary to ensure optimal accuracy.",
+    ],
+  },
+
+  {
+    title: "Technology Engineer",
+    company_name: "BooleanLabs",
+    icon: booleanlabs,
+    iconBg: "#383E56",
+    date: "Jul 2021 - Feb 2023",
+    points: [
+      "Develop and maintain Node.js applications to create robust and scalable APIs.",
+      "Design and implement APIs for various functionalities, ensuring proper request handling and response formatting.",
+      "Integrate payment gateways into applications, facilitating secure and seamless payment processing",
+      "Utilize GCP services and APIs to integrate cloud-based functionalities into Node.js applications.",
+      "Manage code repositories using Bitbucket, ensuring version control and collaborative development.",
+      "Implement Swagger documentation for APIs, providing clear and comprehensive API specifications.",
+      "Collaborate with cross-functional teams to understand requirements and design appropriate API solutions.",
+      "Perform testing and debugging of APIs to ensure functionality, security, and performance.",
+      "Ensure proper error handling and logging mechanisms in API implementations.",
+      "Provide support and assistance to other developers in API integration and usage."
+    ],
+  },
+
+  
+  {
+    title: "Software Engineer Intern",
+    company_name: "AXCITO",
+    icon: axcito,
+    iconBg: "#FFFFFF",
+    date: "Mar 2021 - Jun 2021",
+    points: [
+      "Develop and maintain web applications using the Laravel framework, ensuring high-quality code and optimal performance.",
+      "Implement SEO best practices, optimizing web applications for search engine visibility and rankings.",
+      "Collaborate with cross-functional teams to understand SEO requirements and implement appropriate strategies.",
+      "Utilize GitLab for version control, managing code repositories, and facilitating collaborative development.",
+      "Create and maintain comprehensive documentation for web applications, including user manuals, technical guides, and API documentation.",
+      "Conduct regular audits of documentation to identify areas for improvement and implement updates as needed.",
+      "Work closely with developers, designers, and other stakeholders to gather information and create clear and concise documentation.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "MaBaby",
+    description:
+      "Mobile-based platform that allows users to monitor and manage pregnancy status effortlessly. Track fetal development, health, and appointments. Stay connected to a smooth and informed pregnancy journey.",
+    tags: [
+      {
+        name: "NodeJs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+    ],
+    image: mababy,
+    source_code_link:{
+      type: "url",
+      link: "https://apps.apple.com/lk/app/mababy-mobile/id1670050279",
+    }
+  },
+  {
+    name: "HDIP",
+    description:
+      "A web application that enables users to monitor and control refrigeration units, optimizing efficiency and reducing energy consumption while ensuring environmental sustainability.",
+    tags: [
+      {
+        name: "MERN",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Oauth",
+        color: "green-text-gradient",
+      },
+      {
+        name: "IOT",
+        color: "pink-text-gradient",
+      },
+    ],
+    // image: jobit,
+    source_code_link: {
+      type: "url",
+      link: "http://18.118.67.1/login",
+    },
+  },
+  {
+    name: "4xtra",
+    description:
+      "A web application that facilitates the analysis of synthetic data.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "React-Chartjs-2",
+        color: "green-text-gradient",
+      },
+    ],
+    image: xtra,
+    source_code_link:{
+      type: "url",
+      link: "http://sdg.4-xtra.com:8443/4-xtra",
+    }
+  },
+  {
+    name: "SmartScience",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "React-Chartjs-2",
+        color: "green-text-gradient",
+      },
+    ],
+    image: smartscience,
+    source_code_link:{
+      type: "url",
+      link: "https://www.smartscience.online/",
+    }
+  },
+  {
+    name: "Rotunda",
+    description:
+      "User-friendly website, allowing you to design and construct intricate models, explore different styles, and bring your architectural dreams to life",
+    tags: [
+      {
+        name: "MERN",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "GCP",
+        color: "green-text-gradient",
+      },
+    ],
+    image: rotunda,
+    source_code_link:{
+      type: "url",
+      link: "http://rotunda360.com/",
+    }
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
